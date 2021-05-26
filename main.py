c91ae8d3fb6892858af22f9d4be71c9430e39a8f
TanviK1 <>
2021-05-26 22:09:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+num1 = 20
+num2 = 2
+
+print ("num1 =", num1)
+print("num2=", num2)
+
+print("num1=", num1, "num2=", num2)
+
+
+add = num1 + num2
+sub = num1 - num2
+multiply = num2 * num1
+
+print("addition =", add)
+print("sub=", sub)
+print("multiply = " , multiply)
